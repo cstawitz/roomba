977515a5a93f1322f0ec7cf272a3ee516bb403d4
aedobbyn <adobbs11@gmail.com>
2018-05-21 16:37:47 -0500

    add roomba-package.R

--- R/rrroomba.R
+++ R/rrroomba.R
@@ -1,32 +1,3 @@
-#' roomba package
-#'
-#' \tabular{ll}{
-#' Package: \tab roomba\cr
-#' Type: \tab Package\cr
-#' }
-#'
-#' Collection of functions to do deal with deeply nested data
-#'
-#' @name roomba-package
-#' @docType package
-NULL
-
-#' Example Twitter data
-#'
-#' Example data from Twitter API
-#'
-#' @name twitter_data
-#' @rdname example_data
-#' @docType data
-#' @keywords twitter data
-#' @examples
-#' twitter_data[[1]][["id"]]
-NULL
-
-
-
-
-
 #' Roomba
 #'
 #' @description Tidy your nested list
@@ -44,7 +15,6 @@ NULL
 #'
 #'
 #'
-
 
 roomba <- function(inp, replacement = NA_character_,
                    rows = NULL, cols = NULL) {
