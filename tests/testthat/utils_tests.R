93be79b80c493c5a4085b2c215f24f3f78755a0e
aedobbyn <adobbs11@gmail.com>
2018-05-21 17:10:07 -0500

    test for replace_nulls

---
+++ tests/testthat/utils_tests.R
@@ -0,0 +1,7 @@
+
+# Test that replace_null() works as expected
+lst <- list(c("a", "b"), x = NULL)
+cleaned_lst <- purrr::map(vec, replace_null, replacement = "foo")
+testthat::expect_equal(cleaned_lst,
+                       list(c("a", "b"), x = "foo"))
+
