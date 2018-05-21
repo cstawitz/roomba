907ab30cb1085e797d1f8ec50b57c26280d7f83d
aedobbyn <adobbs11@gmail.com>
2018-05-21 16:15:24 -0500

    add checks for null arguments

--- R/roomba.R
+++ R/roomba.R
@@ -2,6 +2,7 @@
 #'
 #' @description Tidy your nested list
 #' @param inp Element to replace
+#' @param clean Should NULL values be changed to NAs?
 #' @param replacement Replacement for NULL values. Defaults to NA_character_.
 #' @param rows
 #' @param cols
@@ -20,6 +21,14 @@ roomba <- function(inp, replacement = NA_character_,
                    rows = NULL, cols = NULL) {
   assertthat::assert_that(length(inp),
                           "Input is of length 0.")
+
+  assertthat::assert_that(!is.null(rows) && !is.null(cols),
+                          "rows and cols must both be non-NULL.")
+
+  if (clean == TRUE) {
+    assertthat::assert_that(!is.null(replacement),
+                            "If clean is TRUE, a non-NULL replacement must be provided.")
+  }
 
 
 }
