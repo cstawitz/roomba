56ee5aabf40168c172096b4fbb2f4e32863c8c7e
Jim Hester <james.f.hester@gmail.com>
2018-05-21 14:40:48 -0700

    Fix R CMD check

--- R/roomba.R
+++ R/roomba.R
@@ -23,29 +23,28 @@ NULL
 #' twitter_data[[1]][["id"]]
 NULL
 
-
-
 #' Roomba
 #'
 #' @description Tidy your nested list
-#' @param x Element to replace
-#' @param replacement Replacement for NULL values. Defaults to NA_character_.
 #' @param inp Element to replace
 #' @param clean Should NULL values be changed to NAs?
 #' @param replacement Replacement for NULL values. Defaults to NA_character_.
-#' @param rows
-#' @param cols
+#' @param rows rows to iterate over
+#' @param cols columns to include
 #'
 #' @export
-#'
-#' @examples
-#'
-#'
-#'
+roomba <- function(inp, replacement = NA_character_,
+  rows = NULL, cols = NULL, clean = TRUE) {
+  assertthat::assert_that(length(inp),
+    "Input is of length 0.")
 
+  assertthat::assert_that(!is.null(rows) && !is.null(cols),
+    "rows and cols must both be non-NULL.")
 
-roomba <- function(x, replacement = NA_character_) {
-  assertthat::assert_that()
+  if (clean == TRUE) {
+    assertthat::assert_that(!is.null(replacement),
+      "If clean is TRUE, a non-NULL replacement must be provided.")
+  }
 }
 
 #' Perform a recursive depth first search of a function
@@ -69,22 +68,3 @@ dfs_idx <- function(.x, .f) {
   walk(.x, integer())
   res
 }
-
-
-
-roomba <- function(inp, replacement = NA_character_,
-                   rows = NULL, cols = NULL) {
-  assertthat::assert_that(length(inp),
-                          "Input is of length 0.")
-
-  assertthat::assert_that(!is.null(rows) && !is.null(cols),
-                          "rows and cols must both be non-NULL.")
-
-  if (clean == TRUE) {
-    assertthat::assert_that(!is.null(replacement),
-                            "If clean is TRUE, a non-NULL replacement must be provided.")
-  }
-
-
-}
-
