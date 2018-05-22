7a322d2364ac4a30ce27ab41b564b301f10f2872
Christine Stawitz <cstawitz@uw.edu>
2018-05-22 14:29:11 -0700

    Get silly date example plot

--- inst/Shiny/server.R
+++ inst/Shiny/server.R
@@ -2,6 +2,8 @@
 server <- function(input, output) {
   require(jsonlite)
   require(tools)
+  library(roomba)
+  require(ggplot2)
 
   # Allow data to change as input changes
   datasetInput <- reactive({
@@ -23,8 +25,13 @@ server <- function(input, output) {
   })
   
   output$plot <- eventReactive(input$makePlot, 
-                  { 
+                  {
                     dataset <- datasetInput()
-                    thedata <-roomba::roomba(dataset, input$Variables) 
-                    renderPlot(thedata)})
+                    thedata <-roomba::roomba(dataset, input$Variables)
+                    thedata$created_at <- 
+                      as.POSIXct(thedata$created_at, format="%a %b %d %H:%M:%S %z %Y")
+                    
+                    output$plot <- renderPlot(
+                       ggplot(thedata) + geom_point(aes(x=name, y=created_at))
+                      )})
 }
