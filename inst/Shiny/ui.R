c7aa661bb15ed0ff5011877cf35769d8dc74d059
Christine Stawitz <cstawitz@uw.edu>
2018-05-22 11:39:09 -0700

    Fix ui

--- inst/Shiny/ui.R
+++ inst/Shiny/ui.R
@@ -7,13 +7,13 @@ ui <- fluidPage(
         
         # Input: Slider to select JSON file ----
         fileInput(inputId = "data",
-                  label = "Select a JSON file to import:",
-                  accept = c('.json', '.rda'))
+                  label = "Select a JSON or Rda file to import:",
+                  accept = c('.json', '.rda'), multiple=FALSE),
         
         # Output: Names of first level list objects----
         
-           #conditionalPanel(condition="length(input.data)>0",
-            #fluidRow(column(4,verbatimTextOutput("names")))
+           conditionalPanel(condition="length(input.data)>0",
+            fluidRow(column(4,verbatimTextOutput("names"))))
           ),
       mainPanel()
     )
