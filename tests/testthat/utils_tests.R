5df3b810aeca7b8884053dad5d6cf4c148607160
Jim Hester <james.f.hester@gmail.com>
2018-05-22 09:53:29 -0700

    Fix most R CMD check issues

--- tests/testthat/utils_tests.R
+++
@@ -1,8 +0,0 @@
-context("roomba utility function tests")
-
-# Test that replace_null() works as expected
-lst <- list(c("a", "b"), x = NULL)
-cleaned_lst <- purrr::map(vec, replace_null, replacement = "foo")
-testthat::expect_equal(cleaned_lst,
-                       list(c("a", "b"), x = "foo"))
-
