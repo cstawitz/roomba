eec4f45c4348a9790a7d0bc609d07ac2907ee1ba
Jim Hester <james.f.hester@gmail.com>
2018-05-22 11:40:24 -0700

    Add list_names function

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,9 @@
+#' List all names in a list
+#' @export
+list_names <- function(x, replacement = NA) {
+  name_idx <- dfs_idx(x, ~ length(names(.x)) > 0)
+  unique(unlist(map(name_idx, ~ names(x[[.x]]))))
+}
 
 replace_null <- function(x, replacement = NA) {
   empty_idx <- dfs_idx(x, ~ length(.x) == 0)
