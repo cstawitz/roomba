c0c1786a2ff6e226b903d0ca470fa727dcc4bad0
Laura DeCicco <ldecicco@usgs.gov>
2018-05-22 12:40:56 -0500

    A more useful test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,3 +7,51 @@ test_that("Test that replace_null() works as expected", {
   expect_equal(cleaned_lst,
                list(c("a", "b"), x = "foo"))
 })
+
+test_that("Test that replace_null() works with numbers", {
+  # Test that replace_null() works as expected
+
+  toy_data <- jsonlite::fromJSON('
+  {
+                                 "stuff": {
+                                 "buried": {
+                                 "deep": [
+                                 {
+                                 "goodstuff": "here",
+                                 "name": "Bob Rudis",
+                                 "secret_power": 5,
+                                 "other_secret_power": []
+                                 },
+                                 {
+                                 "goodstuff": "here",
+                                 "name": "Amanda Dobbyn",
+                                 "more_nested_stuff": 4
+                                 }
+                                 ],
+                                 "alsodeep": 2342423234,
+                                 "deeper": {
+                                 "foo": [
+                                 {
+                                 "goodstuff": "not here",
+                                 "name": "barb",
+                                 "secret_power": []
+                                 },
+                                 {
+                                 "goodstuff": "here",    // More deeply nested "goodstuff" than "goodstuff"s above
+                                 "name": "borris"
+                                 }
+                                 ]
+                                 }
+                                 }
+                                 }
+}', simplifyVector = FALSE)
+
+
+  cleaned_lst <- replace_null(toy_data)
+
+  secret_power <- cleaned_lst %>% dfs_idx(~ .x$goodstuff == "here") %>%
+    purrr:::map_dfr(~ cleaned_lst[[.x]]) %>%
+    dplyr::pull(secret_power)
+
+  expect_type(secret_power, "integer")
+})
