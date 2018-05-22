3cb3da7dcc8ba9872d75ce98b6e74eac64c40d2f
Laura DeCicco <ldecicco@usgs.gov>
2018-05-22 14:25:43 -0500

    Getting data to load

--- inst/Shiny/server.R
+++ inst/Shiny/server.R
@@ -2,23 +2,25 @@
 server <- function(input, output) {
   require(jsonlite)
   require(tools)
-  
+
   # Allow data to change as input changes
   datasetInput <- reactive({
     #Determine if input file is .json or .rda
     if(tools::file_ext(input$data$name)=="json") {
-      jsonlite::fromJSON(input$data$datapath)
+      x <- jsonlite::fromJSON(input$data$datapath)
     } else if(tools::file_ext(input$data$name)=="rda"){
       load(input$data$datapath)
+      assign("x", get(ls()))
     }
+    return(x)
   })
-  
+
   #Print names of first level list items
   output$names <- renderTable({
     dataset <- datasetInput()
     roomba::list_names(dataset)
   })
-  
+
   # Show the first "n" observations ----
   #output$view <- renderTable({
   #  head(datasetInput(), n = input$obs)
