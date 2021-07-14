423409745ba8b625748c8f105ca6fa99671a84f7
wcsherb <38059097+wcsherb@users.noreply.github.com>
2021-07-14 13:20:41 -0400

    Using web documentation for magrittr pipe
    the documentation redirect was failing for %>%
    trying to keep it simple

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,6 +1,6 @@
 #' Pipe operator
 #'
-#' See \link[magrittr]{pipe} for details.
+#' See \code{\href{https://rdrr.io/cran/magrittr/man/pipe.html}{magrittr::pipe}} for details.
 #'
 #' @name %>%
 #' @rdname pipe
